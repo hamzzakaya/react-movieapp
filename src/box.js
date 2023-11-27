11f8b192838e8c4298a111977ab5d1c6716a33a8
hamzzakaya <hamzakaya4343@gmail.com>
2023-11-27 20:39:06 +0300

    movieapp

---
+++ src/box.js
@@ -0,0 +1,11 @@
+function Box() {
+    return (
+        <div className="box">
+
+            <img src='https://s3.cloud.ngn.com.tr/kitantik/images/2022-07-16/1br9qfyl5nunxlr1r0t.jpg' />
+            <span className="span1">Film Adı</span>
+            <span className="span2">Film Konusu</span>
+        </div>
+    )
+}
+export default Box
---
+++ src/layout.js
@@ -0,0 +1,26 @@
+import './layout.css'
+import Box from './box'
+
+
+
+function Layout() {
+
+  return (
+    <div className='body'>
+      <div className='layout' >
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
